8e7daa0f166f541c1b6173e9b4005d55f1cf68bf
Kim Seung Wook <wookiekim@postech.ac.kr>
2020-05-15 19:12:43 +0900

    Update k_jump_mst.py
    works, but what if the second lowest is the only edge to a certain vertex?

--- postech_algo/midterm/k_jump_mst.py
+++ postech_algo/midterm/k_jump_mst.py
@@ -1,135 +1,103 @@
-import heapq
-import itertools
 import sys
 
-REMOVED = '<removed-task>'
 
-def dfs(visited, graph, v):
-  visited[v] = 1
+def find(pr, v, inter):
+    if pr[v][0] == v:
+        for subp in inter:
+            pr[subp][0] = v
+            pr[subp][1] = pr[v][1] - 1
+        return v
 
-  if v in graph:
-    for k in graph[v]:
-      if visited[k] == 0:
-        dfs(visited, graph, k)
-
-def add_task(entry_finder, counter, heap, task, priority=0):
-  if task in entry_finder:
-    remove_task(task, entry_finder)
-  count = next(counter)
-  entry = [priority, count, task]
-  entry_finder[task] = entry
-  heapq.heappush(heap, entry)
-
-def remove_task(task, entry_finder):
-  entry = entry_finder.pop(task)
-  entry[-1] = REMOVED
-
-def pop_task(heap, entry_finder):
-  while heap:
-    priority, count, task = heapq.heappop(heap)
-    if task is not REMOVED:
-      del entry_finder[task]
-      return task, priority
-  raise KeyError('pop from an empty priority queue')
-
-def is_ok(added, weight):
-  for w in added:
-    if w > weight:
-      if w - weight < K:
-        return 0
     else:
-      if weight - w < K:
-        return 0
-  return 1
+        inter.append(pr[v][0])
+        return find(pr, pr[v][0], inter)
 
-def MST(graph, N, K):
-  weight_sum = 0
-  added = []
 
-  included = [0] *  N
-  distances = [sys.maxsize] * N
-  total_included = 0
+def union(pr, u, v):
+    if pr[u][1] > pr[v][1]:
+        pr[v][0] = u
+    elif pr[u][1] < pr[v][1]:
+        pr[u][0] = v
+    else:
+        pr[u][0] = v
+        pr[v][1] += 1
 
-  heap = []
-  entry_finder = {}
-  counter = itertools.count()
 
-  graph[0] = {k:v for k,v sorted(graph[0].items(), key=lambda item: item)}
+def kruskal(N, K, edges, edgeinfo):
+    edges.sort(key=lambda edge: edge[2])
+    #edges.sort(key=lambda edge: edgeinfo[edge[0]])
+    MST = []
 
-  included[0] = 1
-  total_included += 1
-  for n in graph[0]:
-    if included[n] == 0:
-      if is_ok(distances, graph[0][n]):
-        add_task(entry_finder, counter, heap, n, graph[0][n])
-        distances[n] = graph[0][n]
+    parents_ranks = []
 
-  while total_included < N:
-    try:
-      temp, weight = pop_task(heap, entry_finder)
-      if included[temp] or not is_ok(added, weight):
-        continue
-      included[temp] = 1
-      total_included += 1
-      weight_sum += weight
-      added.append(weight)
-    except:
-      return "NO"
-    for n in graph[temp]:
-      if included[n] == 0:
-        priority = min(distances[n], graph[temp][n])
-        add_task(entry_finder, counter, heap, n, priority)
-    print(added, included, distancesf)
-  return weight_sum
+    for i in range(N):
+        parents_ranks.append([i, 0])
+
+    curmin = -K
+    for edge in edges:
+        u, v, c = edge
+        if c - curmin < K:
+            continue
+
+        u_inter = []
+        v_inter = []
+        root_u = find(parents_ranks, u, u_inter)
+        root_v = find(parents_ranks, v, v_inter)
+        if root_u != root_v:
+            MST.append(edge)
+            curmin = c
+            if len(MST) == N - 1:
+                break
+            union(parents_ranks, root_u, root_v)
+
+    if len(MST) < N - 1:
+        return "NO"
+    else:
+        wsum = 0
+        for e in MST:
+            wsum += e[2]
+        return wsum
 
 
 def read_ints():
-  temp = input().split()
-  temp = [int(t) for t in temp]
-  return temp
+    temp = input().split()
+    temp = [int(t) for t in temp]
+    return temp
+
 
 tests = int(input())
 
 for _ in range(tests):
-  N,M,K = read_ints() 
-  
-  max_c = 0
-  min_c = sys.maxsize
+    N, M, K = read_ints()
 
-  graph = {}
-  for _ in range(M):
-    u,v,c = read_ints()
+    max_c = 0
+    min_c = sys.maxsize
 
-    if min_c > c:
-      min_c = c
-    elif max_c < c:
-      max_c = c
+    edges = []
+    edgeinfo = [0] * N
+    for _ in range(M):
+        u, v, c = read_ints()
 
-    if u not in graph:
-      graph[u] = {}
-    
-    if v not in graph:
-      graph[v] = {}
+        edgeinfo[u] += 1
+        edgeinfo[v] += 1
 
-    graph[u][v] = c
-    graph[v][u] = c
+        if min_c > c:
+            min_c = c
+        elif max_c < c:
+            max_c = c
 
-  # CHeck if there are enough number of edges
-  if M < N - 1:
-    print("NO")
-    continue
+        edges.append((u, v, c))
+        #edges.append((v,u,c))
 
-  # Check if there are enough edges with K apart
-  if (max_c - min_c) / K  + 1 < N - 1:
-    print("NO")
-    continue
+    # CHeck if there are enough number of edges
+    if M < N - 1:
+        print("NO")
+        continue
 
-  # Check if all nodes are connected
-  visited = [0] * N
-  dfs(visited, graph, 0)
-  if sum(visited) < N:
-    print("NO")
-    continue
+    # Check if there are enough edges with K apart
+    if (max_c - min_c) / K + 1 < N - 1:
+        print("NO")
+        continue
 
-  result = MST(graph, N, K)
-  print(result)
+    result = kruskal(N, K, edges, edgeinfo)
+    print(result)
