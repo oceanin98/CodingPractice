d9361fa33ed7f90ea4fa46b501bb5c781f0ec218
Kim Seung Wook <wookiekim@postech.ac.kr>
2020-05-15 16:54:41 +0900

    Update k_jump_mst.py
    next version, better optimizations, not sure if things work with prim though

--- postech_algo/midterm/k_jump_mst.py
+++ postech_algo/midterm/k_jump_mst.py
@@ -4,6 +4,14 @@ import sys
 
 REMOVED = '<removed-task>'
 
+def dfs(visited, graph, v):
+  visited[v] = 1
+
+  if v in graph:
+    for k in graph[v]:
+      if visited[k] == 0:
+        dfs(visited, graph, k)
+
 def add_task(entry_finder, counter, heap, task, priority=0):
   if task in entry_finder:
     remove_task(task, entry_finder)
@@ -24,8 +32,19 @@ def pop_task(heap, entry_finder):
       return task, priority
   raise KeyError('pop from an empty priority queue')
 
-def MST(graph, N):
+def is_ok(added, weight):
+  for w in added:
+    if w > weight:
+      if w - weight < K:
+        return 0
+    else:
+      if weight - w < K:
+        return 0
+  return 1
+
+def MST(graph, N, K):
   weight_sum = 0
+  added = []
 
   included = [0] *  N
   distances = [sys.maxsize] * N
@@ -35,29 +54,32 @@ def MST(graph, N):
   entry_finder = {}
   counter = itertools.count()
 
+  graph[0] = {k:v for k,v sorted(graph[0].items(), key=lambda item: item)}
+
   included[0] = 1
   total_included += 1
   for n in graph[0]:
     if included[n] == 0:
-      add_task(entry_finder, counter, heap, n, graph[0][n])
-      distances[n] = graph[0][n]
+      if is_ok(distances, graph[0][n]):
+        add_task(entry_finder, counter, heap, n, graph[0][n])
+        distances[n] = graph[0][n]
 
   while total_included < N:
     try:
       temp, weight = pop_task(heap, entry_finder)
-      if included[temp]:
+      if included[temp] or not is_ok(added, weight):
         continue
       included[temp] = 1
       total_included += 1
       weight_sum += weight
+      added.append(weight)
     except:
       return "NO"
-
     for n in graph[temp]:
       if included[n] == 0:
         priority = min(distances[n], graph[temp][n])
         add_task(entry_finder, counter, heap, n, priority)
-
+    print(added, included, distancesf)
   return weight_sum
 
 
@@ -92,12 +114,22 @@ for _ in range(tests):
     graph[u][v] = c
     graph[v][u] = c
 
+  # CHeck if there are enough number of edges
   if M < N - 1:
     print("NO")
     continue
 
+  # Check if there are enough edges with K apart
   if (max_c - min_c) / K  + 1 < N - 1:
     print("NO")
     continue
-  result = MST(graph, N)
+
+  # Check if all nodes are connected
+  visited = [0] * N
+  dfs(visited, graph, 0)
+  if sum(visited) < N:
+    print("NO")
+    continue
+
+  result = MST(graph, N, K)
   print(result)
