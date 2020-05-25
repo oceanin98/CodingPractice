49d1c9039e99d181cf24cccfa08942c6b7cc7581
Kim Seung Wook <wookiekim@postech.ac.kr>
2020-05-25 14:29:37 +0900

    Create 200524.py

---
+++ techseries/200524.py
@@ -0,0 +1,43 @@
+# Given a string, s, find the longest palindromic substring in s.
+
+class Solution: 
+    def longestPalindrome(self, s):
+      maxlength = 1
+      start = 0
+      for i in range(1,len(s)):
+        # odd length
+        curlen = 1
+        left = right = 1
+        while i - left >= 0 and i + right < len(s):
+          if s[i - left] == s[i + right]:
+            left += 1
+            right += 1
+            curlen += 2
+          else:
+            break
+        if curlen > maxlength:
+          maxlength = curlen
+          start = i - left + 1
+        # even length
+        curlen = 2
+        left = right = 1
+        if s[i] != s[i - 1]:
+          continue
+        while i - left - 1 > 0 and i + right < len(s):
+          if s[i - left - 1] == s[i + right]:
+            left += 1
+            right += 1
+            curlen += 2
+          else:
+            break
+        if curlen > maxlength:
+          maxlength = curlen
+          start = i - left
+      return s[start:start+maxlength]
+       
+        
+# Test program
+ss = ["tracecars", "banana", "million", "aaaaaaaaaaaaaaa"]
+for s in ss:
+  print(str(Solution().longestPalindrome(s)))
+  # racecar
