f2ea976ca7c8421595753ef090f36654cd7a7119
Kim Seung Wook <wookiekim@postech.ac.kr>
2020-06-25 12:23:42 +0900

    Update 122A.py

--- codeforces/122A.py
+++ codeforces/122A.py
@@ -1,10 +1,5 @@
 # https://codeforces.com/problemset/problem/122/A
 
-def read_ints():
-    temp = input().split()
-    ints = [int(t) for t in temp]
-    return ints
-
 divisors = [4,7, 44, 47, 74, 77, 444,447,474,477,744,747,774,777]
 
 n = int(input())
