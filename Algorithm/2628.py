8b5d9806b13724bb317b246a2043ba1ceb523565
haein <oeanin2088@gmail.com>
2022-11-13 11:17:55 +0900

    11/13

--- Algorithm/2628.py
+++ Algorithm/2628.py
@@ -23,3 +23,29 @@ for i in range(len(column)-1):
     subtracted_c.append(column[i+1]-column[i])
     
 print(max(subtracted_r)*max(subtracted_c))
+
+
+
+#다른버전 ####################
+# x,y= map(int,input().split())
+# x_list= [0,x]
+# y_list=[0,y]
+# for _ in range(int(input())):
+#     xy, length= map(int,input().split())
+#     if xy ==0:
+#         y_list.append(length)
+#     else:
+#         x_list.append(length)
+
+# x_list.sort()
+# y_list.sort()
+
+# max_square=0
+# for i in range(1,len(x_list)):
+#     for j in range(1, len(y_list)):
+#         width= x_list[i]-x_list[i-1]
+#         height= y_list[j]- y_list[j-1]
+#         max_square= max(max_square, width,*height)
+
+# print(max_square) 
+        
